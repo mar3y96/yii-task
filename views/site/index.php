919701de5905cc46ce18650c41a7d6a8cb3de43b
mohammed marey <mohammedmaray9674@gmail.com>
2023-05-02 16:02:26 +0200

    add digram ,edit landing page

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,34 @@
 <?php
 
-/** @var yii\web\View $this */
+	/** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+	use yii\helpers\Html;
+
+	$this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
+	<div class="jumbotron text-center bg-transparent">
+		<h1 class="display-4">Welcome!</h1>
+	</div>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+	<div class="body-content">
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+		<div class="row ">
+			<div class="col-lg-6">
 
-    <div class="body-content">
+				<p>
+					<?= Html::a('Categories', ['/category/index'], ['class' => 'btn btn-lg btn-success'])
+					?></p>
+			</div>
+			<div class="col-lg-6 d-flex flex-row-reverse bd-highlight">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+				<p>
+					<?= Html::a('Products', ['/product/index'], ['class' => 'btn btn-lg btn-info']) ?>
+				</p>
+			</div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+		</div>
 
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+	</div>
 </div>
