4fab0a44e4c36d9aa057a57a536d7420fc6fef8c
mohammed marey <mohammedmaray9674@gmail.com>
2023-05-02 09:48:46 +0200

    generate controllers

---
+++ views/product/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use app\models\Product;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/** @var yii\web\View $this */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Products';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'description:ntext',
+            'image',
+            'category_id',
+            //'created_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Product $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
